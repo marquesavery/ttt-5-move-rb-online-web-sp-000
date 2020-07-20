d2428f1e095281cf765f7f6f2f0ab014f040f925
Marques Avery <averymarques4@gmail.com>
2020-07-20 00:58:52 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -7,7 +7,7 @@ def array_adder(a)
   a << "new thing at the end of the array"
 end
 
-z = [1, 'hi', "Byron"]
+z = [1, 'hi', "Byron"]=
 puts "Before call #{z}"
 array_adder(z)
 puts "After call #{z}: Holy moly, *changed*!"
