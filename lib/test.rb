c2e910c004eb543752ed02e07747abb4cb115175
Marques Avery <averymarques4@gmail.com>
2020-07-20 01:09:46 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -2,10 +2,19 @@
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 def move(board, index, value = "X")
   def update_array_at_with(array, index, value)
     array[index] += value
   end
 end
 
-move(board, 2)
+#move(board, 2)
+display_board(move(board, 2))
