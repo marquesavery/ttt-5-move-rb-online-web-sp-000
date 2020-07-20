eb73ae2f9219ad69f72605b22e429901855b07bc
Marques Avery <averymarques4@gmail.com>
2020-07-20 01:13:07 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -14,8 +14,7 @@ def move(board, index, value = "X")
   def update_array_at_with(array, index, value)
     array[index] += value
   end
-  display_board(array)
+  display_board(board)
 end
 
 #move(board, 2)
-move(board,0)
